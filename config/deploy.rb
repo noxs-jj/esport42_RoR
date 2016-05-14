49405541833cc5f178ba47fc97c8ab46b371503d
Jean-Jacques MOIROUX <jeanjacquesmoiroux@hotmail.com>
2016-05-14 13:43:15 +0200

    fix cap seed

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,7 +48,7 @@ namespace :deploy do
 
   desc "reload the database with seed data"
   task :seed do
-    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+    run "cd #{current_path}; bundle exec rake db:seed"
   end
 
   desc 'Restart application'
