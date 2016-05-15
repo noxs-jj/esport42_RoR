09cafd972f20b960102476a1aa8d8c3c269e7a51
Jean-Jacques MOIROUX <jeanjacquesmoiroux@hotmail.com>
2016-05-15 19:14:22 +0200

    backoffice event create

--- app/controllers/backoffice/event_controller.rb
+++ app/controllers/backoffice/event_controller.rb
@@ -1,5 +1,6 @@
 class Backoffice::EventController < Backoffice::ApplicationController
   def index
+    @events = Event.all
   end
 
   def show
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ActiveRecord::Base
+  def user_name
+    result = User.unscoped.find(self.created_by).username
+    return result if !result.blank?
+    ""
+  end
 end
