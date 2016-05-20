0690b79ccb12e0016e7bcdee6527cce22d3d6380
Jean-Jacques MOIROUX <jeanjacquesmoiroux@hotmail.com>
2016-05-20 21:37:32 +0200

    fin de journee

--- app/helpers/bracket_helper.rb
+++ app/helpers/bracket_helper.rb
@@ -1,4 +1,28 @@
 module BracketHelper
+  def bracketHelper_column_and_space(cells_to_spawn, space_between_cells)
+    ap "DEBBB"
+    start = space_between_cells / 2
+    ap "start #{start}"
+    i = 0
+    i_cell = 0
+
+    render partial: 'backoffice/bracket/bracket_space_cell'
+    while i < start
+        render partial: 'backoffice/bracket/bracket_empty_cell'
+        i += 1
+    end
+
+    while i_cell < cells_to_spawn
+      render partial: 'backoffice/bracket/bracket_cell'
+      i = 0
+      # while i < start
+      #     render partial: 'backoffice/bracket/bracket_empty_cell'
+      #     i += 1
+      # end
+      i_cell += 1
+    end
+  end
+
   def bracketHelper_number_column_with_number_participants(participant_nbr)
     return 1 if participant_nbr <= 2
     return 2 if participant_nbr <= 4
